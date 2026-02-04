947daf2cdecdd35828dc5026a743180a6569e38b
rd <rohandas.zero@gmail.com>
2026-02-04 18:14:35 +0530

    use pinterest imgs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        port: '',
+        pathname: '/**', // Allows images from any path on this host
+      },
+    ],
+  },
 };
 
 export default nextConfig;
